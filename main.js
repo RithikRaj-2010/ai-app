a6f2fef327b96a57e87335bd9bda4205b0cf7251
RithikRaj-2010 <84604990+RithikRaj-2010@users.noreply.github.com>
2022-08-28 16:08:37 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+    song=loadSound("music.mp3");
+    }
+    function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on("pose",gotPoses);
+    }
+    function draw(){
+        image(video,0,0,600,500);
+        }
